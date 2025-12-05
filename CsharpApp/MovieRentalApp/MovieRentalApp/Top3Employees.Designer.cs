68c2fac7e9066c2873c7742b82b3ed7718ce3b80
danish kumar <danishd@mymacewan.ca>
2025-12-04 22:07:09 -0700

    top3employees fixed

--- CsharpApp/MovieRentalApp/MovieRentalApp/Top3Employees.Designer.cs
+++ CsharpApp/MovieRentalApp/MovieRentalApp/Top3Employees.Designer.cs
@@ -41,9 +41,10 @@
             // 
             this.monthLabel.AutoSize = true;
             this.monthLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.monthLabel.Location = new System.Drawing.Point(36, 19);
+            this.monthLabel.Location = new System.Drawing.Point(27, 15);
+            this.monthLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.monthLabel.Name = "monthLabel";
-            this.monthLabel.Size = new System.Drawing.Size(73, 25);
+            this.monthLabel.Size = new System.Drawing.Size(58, 20);
             this.monthLabel.TabIndex = 0;
             this.monthLabel.Text = "Month:";
             // 
@@ -51,61 +52,69 @@
             // 
             this.yearLabel.AutoSize = true;
             this.yearLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.yearLabel.Location = new System.Drawing.Point(296, 19);
+            this.yearLabel.Location = new System.Drawing.Point(222, 15);
+            this.yearLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.yearLabel.Name = "yearLabel";
-            this.yearLabel.Size = new System.Drawing.Size(59, 25);
+            this.yearLabel.Size = new System.Drawing.Size(47, 20);
             this.yearLabel.TabIndex = 1;
             this.yearLabel.Text = "Year:";
             // 
             // monthComboBox
             // 
             this.monthComboBox.FormattingEnabled = true;
-            this.monthComboBox.Location = new System.Drawing.Point(115, 19);
+            this.monthComboBox.Location = new System.Drawing.Point(86, 15);
+            this.monthComboBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.monthComboBox.Name = "monthComboBox";
-            this.monthComboBox.Size = new System.Drawing.Size(136, 24);
+            this.monthComboBox.Size = new System.Drawing.Size(103, 21);
             this.monthComboBox.TabIndex = 2;
             // 
             // yearComboBox
             // 
             this.yearComboBox.FormattingEnabled = true;
-            this.yearComboBox.Location = new System.Drawing.Point(361, 20);
+            this.yearComboBox.Location = new System.Drawing.Point(271, 16);
+            this.yearComboBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.yearComboBox.Name = "yearComboBox";
-            this.yearComboBox.Size = new System.Drawing.Size(148, 24);
+            this.yearComboBox.Size = new System.Drawing.Size(112, 21);
             this.yearComboBox.TabIndex = 3;
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(526, 16);
+            this.button1.Location = new System.Drawing.Point(394, 13);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(60, 35);
+            this.button1.Size = new System.Drawing.Size(45, 28);
             this.button1.TabIndex = 4;
             this.button1.Text = "GO";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.goButton_Click);
             // 
             // topEmployeesDataGridView
             // 
             this.topEmployeesDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.topEmployeesDataGridView.Location = new System.Drawing.Point(41, 57);
+            this.topEmployeesDataGridView.Location = new System.Drawing.Point(31, 46);
+            this.topEmployeesDataGridView.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.topEmployeesDataGridView.Name = "topEmployeesDataGridView";
             this.topEmployeesDataGridView.RowHeadersWidth = 51;
             this.topEmployeesDataGridView.RowTemplate.Height = 24;
-            this.topEmployeesDataGridView.Size = new System.Drawing.Size(468, 159);
+            this.topEmployeesDataGridView.Size = new System.Drawing.Size(351, 129);
             this.topEmployeesDataGridView.TabIndex = 5;
             // 
             // Top3Employees
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(603, 241);
+            this.ClientSize = new System.Drawing.Size(452, 196);
             this.Controls.Add(this.topEmployeesDataGridView);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.yearComboBox);
             this.Controls.Add(this.monthComboBox);
             this.Controls.Add(this.yearLabel);
             this.Controls.Add(this.monthLabel);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Top3Employees";
             this.Text = "Top3Employees";
+            this.Load += new System.EventHandler(this.Top3Employees_Load);
             ((System.ComponentModel.ISupportInitialize)(this.topEmployeesDataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
